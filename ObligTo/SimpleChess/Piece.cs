09afa91d6177a7e3e10faf99bd9d7fdb8120407e
Danica Ellefsen Kvilhaug <65165280+Gloxie@users.noreply.github.com>
2020-10-21 14:06:07 +0200

    Finished Oblig 2

---
+++ ObligTo/SimpleChess/Piece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleChess
+{
+    abstract class Piece
+    {
+        public string Symbol { get; }
+
+        protected Piece(string symbol)
+        {
+            Symbol = symbol;
+        }
+
+        public abstract bool Move(string fromPosition, string toPosition);
+    }
+}
---
+++ ObligTo/SimpleChess/Rook.cs
@@ -0,0 +1,15 @@
+namespace SimpleChess
+{
+    class Rook : Piece
+    {
+        public Rook() : base("TRN")
+        {
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+
+        }
+    }
+}
